bbfde0e7fb17c70219a058cbdfb8237c0dd1b89f
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-01-13 12:13:35 +0800

    zy: fix spec putting init in beforeeach

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -34,46 +34,54 @@ describe("player", function(){
 
     describe('game spec with **被打败了', function() {
         var game;
+        var result;
+        var player_a;
+        var player_b;
 
-        beforeEach(function () {
-            var player_a = new Player(player_a_info);
-            var player_b = new Player(player_b_info);
-            game = new Game(player_a, player_b);
+        beforeEach(function() {
+            player_a = new Player(player_a_info);
+            player_b = new Player(player_b_info);
         });
 
-        it('game spec with 李四被打败了', function(){
-            var result = game.start().lose_msg;
+        it('player_a PK player_b: 李四被打败了', function(){
+            game = new Game(player_a, player_b);
+            result = game.start().lose_msg;
 
             assert.equal(result, '李四被打败了.');
+        });
+
+        it('player_b PK player_a: 张三被打败了', function(){
+            game = new Game(player_b, player_a);
+            result = game.start().lose_msg;
+
+            assert.equal(result, '张三被打败了.');
         });
     });
 
     describe('game spec with **攻击了**,**受到了*点伤害,**剩余生命：*...**被打败了', function () {
-        describe('player_a PK player_b, player_a PK player_c: ', function () {
-            it('player_a and player_b: game spec with 张三攻击了李四,李四受到了1点伤害,李四剩余生命：0\n' +
-            '李四被打败了.', function(){
-                var player_a = new Player(player_a_info);
-                var player_b = new Player(player_b_info);
-                var game_msg = new Game(player_a, player_b).start();
+        it('player_a PK player_b: 张三攻击了李四,李四受到了1点伤害,李四剩余生命：0\n' +
+        '李四被打败了.', function(){
+            var player_a = new Player(player_a_info);
+            var player_b = new Player(player_b_info);
+            var game_msg = new Game(player_a, player_b).start();
 
-                var result = game_msg.attack_process + game_msg.lose_msg;
+            var result = game_msg.attack_process + game_msg.lose_msg;
 
-                assert.equal(result, '张三攻击了李四,李四受到了1点伤害,李四剩余生命：0\n李四被打败了.')
-            });
+            assert.equal(result, '张三攻击了李四,李四受到了1点伤害,李四剩余生命：0\n李四被打败了.')
+        });
 
-            it('player_a and player_c: game spec with 张三攻击了李四,李四受到了1点伤害,李四剩余生命：1\n' +
+        it('player_a PK player_c: 张三攻击了李四,李四受到了1点伤害,李四剩余生命：1\n' +
+        '李四攻击了张三,张三受到了1点伤害,张三剩余生命：0\n' +
+        '张三被打败了.', function(){
+            var player_a = new Player(player_a_info);
+            var player_c = new Player(player_c_info);
+            var game_msg = new Game(player_a, player_c).start();
+
+            var result = game_msg.attack_process + game_msg.lose_msg;
+
+            assert.equal(result, '张三攻击了李四,李四受到了1点伤害,李四剩余生命：1\n' +
             '李四攻击了张三,张三受到了1点伤害,张三剩余生命：0\n' +
-            '张三被打败了.', function(){
-                var player_a = new Player(player_a_info);
-                var player_c = new Player(player_c_info);
-                var game_msg = new Game(player_a, player_c).start();
-
-                var result = game_msg.attack_process + game_msg.lose_msg;
-
-                assert.equal(result, '张三攻击了李四,李四受到了1点伤害,李四剩余生命：1\n' +
-                '李四攻击了张三,张三受到了1点伤害,张三剩余生命：0\n' +
-                '张三被打败了.');
-            });
+            '张三被打败了.');
         });
     });
 
